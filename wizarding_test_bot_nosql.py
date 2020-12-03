8d653362e9ab7b9f2823c1e4f60e87645318434c
EC2 Default User <ec2-user@ip-172-31-30-50.us-east-2.compute.internal>
2020-12-03 16:15:48 +0000

    nosql base is running

--- wizarding_test_bot_nosql.py
+++ wizarding_test_bot_nosql.py
@@ -1,6 +1,16 @@
+#!/tmp/reddit_bot_app/lib/python3.7/site-packages/
+# coding: utf-8
+#from /tmp/dumbledore-reddit-app/lib/python3.7/site-packages/praw import praw
 import praw
+#praw.path.insert(0, 'tmp/dumbledore-reddit-app/lib/python3.7/site-packages/')
+import pdb
+import re
 import os
 import sys
+import time
+import random
+import sqlite3
+from sqlite3 import Error
 from database.firestore_db import FirestoreDatabase
 
 
@@ -9,27 +19,35 @@ def getGuideLinkText():
 
 def search(reddit, db, posts_replied_to):
     for results in reddit.subreddit('thetestingzone').comments():
-        if results.id not in posts_replied_to:
+        #if results.id not in posts_replied_to:
+        if not results.saved:
+            results.save()
             body = results.body  # Grab the Comment
             body = body.lower()	
             found = body.find('!redditgalleon')
+            if found != -1:
+                try:
+                    results.reply("bot is running")
+                except:
+                    break
 
 def main():
     assert os.environ['HP_FIREBASE_CREDS']
     db = FirestoreDatabase(os.environ['HP_FIREBASE_CREDS'])
-    reddit = praw.Reddit('ww-test-bot')
+    reddit = praw.Reddit(client_id="-4ByKCJuYOID4A", client_secret="we-8e_TZOCFsHmhmF8rZEPJTWLI", password="&2@X3G5BX6*v5K%a", username="ww-test-bot", user_agent="ww-test 1.0")
     
-    if not os.path.isfile("posts_replied_to.txt"):
-        posts_replied_to = []
-    else:
-        with open("posts_replied_to.txt", "r") as f:
-            posts_replied_to = f.read()
-            posts_replied_to = posts_replied_to.split("\n")
+    # if not os.path.isfile("posts_replied_to.txt"):
+    #     posts_replied_to = []
+    # else:
+    #     with open("posts_replied_to.txt", "r") as f:
+    #         posts_replied_to = f.read()
+    #         posts_replied_to = posts_replied_to.split("\n")
 
-    # search(reddit, db, posts_replied_to)
+    posts_replied_to = 2
+    search(reddit, db, posts_replied_to)
     
-    with open("posts_replied_to.txt", "w") as f:
-        for post_id in posts_replied_to:
-            f.write(post_id + "\n")
+    # with open("posts_replied_to.txt", "w") as f:
+    #     for post_id in posts_replied_to:
+    #         f.write(post_id + "\n")
 
 main()
